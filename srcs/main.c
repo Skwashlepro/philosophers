8e9624d09e675b5db9e16beb7ec3c25cf8fd0928
Skwashlepro <skwashlepro@gmail.com>
2025-01-29 23:34:03 +0100

    all

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: luctan <luctan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 20:41:19 by luctan            #+#    #+#             */
-/*   Updated: 2025/01/24 20:14:57 by luctan           ###   ########.fr       */
+/*   Updated: 2025/01/29 23:33:27 by luctan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ int	main(int ac, char **av)
 {
 	t_table	table;
 
-	if (inputchk(av))
-		printf("Invalid arguments\n");
-	else if ((ac == 6 || ac == 5))
+	if ((ac == 6 || ac == 5))
 	{
+		if (inputchk(av))
+			return (printf("Invalid arguments\n"), 1);
 		db_init(&table, av + 1);
 		sim_init(&table);
 		db_free(&table);
 	}
 	else
-		printf("Invalid number of argumentes\n");
+		printf("Invalid number of arguments\n");
 	return (0);
 }
