5938f23b43654c435bbfd4cbb321ad17886d5123
Skwashlepro <skwashlepro@gmail.com>
2025-02-03 21:44:25 +0100

    usleep to 2ms seems OK

--- srcs/sim/monitoring.c
+++ srcs/sim/monitoring.c
@@ -6,7 +6,7 @@
 /*   By: luctan <luctan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 21:43:04 by luctan            #+#    #+#             */
-/*   Updated: 2025/02/03 21:43:17 by luctan           ###   ########.fr       */
+/*   Updated: 2025/02/03 21:43:59 by luctan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ void	*sim_monitor(void *data)
 				print_stat(DEAD, table->philos + i);
 			}
 		}
-		usleep(5000);
+		usleep(2000);
 	}
 	return (NULL);
 }
